2d9519fc72832bd455048132cb931be367d2618f
Pedro Javier Nicolás Zamora <pedrojavier.nz@gmail.com>
2018-08-27 16:45:44 +0200

    Init project and get the subjects

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # um-resource-scraper
 A web scraper for obtaining the resources of your profile in the University of Murcia.
+
+# English
+
+The english docs are not available yet. You can contribute and make a PR to help!
+
+# Spanish
+
+## Introducción
+
+Con `um-resource-scraper` puedes descargarte todos los recursos de todas tus asignaturas del **Aula virtual** con tan solo un par de clicks. Cualquier aportación al proyecto es bien recibida.
+
+Para hacer esto posible el programa utiliza un _navegador headless_ basado en google chrome, con el que se hace login en el portal de entrada de la universidad y con el que se va recorriendo las distintas páginas de recursos de cada asignatura.
+
+Ya que el software se basa en un _web scraper_ para obtener los recursos, podrá dejar de funcionar siempre que se actualice cualquier página de la Universidad de Murcia.
+
+## Aviso importante
+
+El autor de este software no se hace responsable de cualquier uso ilegal o poco ético que se pueda hacer de él. El programa está diseñado únicamente con fines educativos. Al usarlo, el usuario es el único responsable de las posibles consecuencias. El software está publicado bajo la licencia MIT y no se ofrece ninguna garantía de su funcionamiento.
+
+## ¿Por qué tengo que introducir mi contraseña?
+
+Los recusos de cada asignatura del aula virtual están protegidos, y tan solo los alumnos matricuados pueden acceder a ellos. Por eso, sin hacer un login previo, es imposible poder descargar dichos recursos. Tu contraseña no será almacenada en ningún sitio, y los recursos que descargues solo serán almacenados en tu ordenador.
+
+## Instalación
+
+Para descargar e instalar las dependencias del programa, necesitas tener instalado `git`, `node` y `npm` en tu sistema:
+
+```
+git clone https://github.com/dizzyrobin/um-resource-scraper
+cd um-resource-scraper
+npm install
+```
+
+## Ejecución
+
+Para ejecutar el programa:
+
+```
+npm start
+```
+
+## ¿Qué pasa si no tengo instalado eso de "node", o "git"?
+
+Ahora mismo, tanto `node` como `git` son requisitos indispensables para la instalación y ejecución del programa. En alguna versión más adelante se distribuirán binarios para los principales sistemas operativos.
---
+++ index.js
@@ -0,0 +1,93 @@
+const puppeteer = require('puppeteer');
+const {user, password} = require('./key');
+
+const WAIT_LONG = 5000;
+const WAIT_NORMAL = 2000;
+const WAIT_SHORT = 1000;
+
+const UM_LOGIN_PAGE = 'https://entrada.um.es/cas/login';
+const UM_AULA_PAGE = 'https://aulavirtual.um.es/portal/login';
+
+const waitMs = async ms => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  })
+};
+
+const getSubjects = async () => {
+  console.log('Starting the headless browser...');
+  const browser = await puppeteer.launch();
+
+  console.log('Loading the login page...');
+  const page = await browser.newPage();
+  await page.goto(UM_LOGIN_PAGE);
+
+  console.log('Waiting for JS...');
+  await waitMs(WAIT_LONG);
+
+  console.log('Setting the "username" and "password" fields...');
+  const pageForm = await page.$('#fm1');
+
+  if (pageForm === null) {
+    throw new Error('Couldn\'t find the page form');
+  }
+
+  const pageUsername = await pageForm.$('#username');
+  const pagePassword = await pageForm.$('#password');
+  const pageSubmit = await pageForm.$('.btn-submit');
+
+  if (pageUsername === null || pagePassword === null || pageSubmit === null) {
+    throw new Error('Cound\'t find the user/password/submit elements');
+  }
+
+  console.log('Logging in...');
+  await pageUsername.type(user);
+  await pagePassword.type(password);
+  await pageSubmit.click();
+  await waitMs(WAIT_LONG);
+  await page.goto(UM_AULA_PAGE);
+
+  console.log('Waiting for the JS...');
+  await waitMs(WAIT_LONG);
+
+  console.log('Getting the subjects...');
+  const leftCol = await page.$('.moresites-left-col');
+
+  if (leftCol === null) {
+    throw new Error('Couldn\'t find the left column...');
+  }
+
+  const sites = await leftCol.$$('.fav-title');
+
+  if (leftCol === null) {
+    throw new Error('Couldn\'t find the fav-title...');
+  }
+
+  for (const i in sites) {
+    const site = await sites[i].$eval('a', e => {
+      const resourceLink = e.getAttribute('href').split('/');
+      const title = e.getAttribute('title');
+
+      return {
+        resource: resourceLink[resourceLink.length - 1],
+        title,
+      };
+    });
+    sites[i] = site;
+  }
+
+  console.log('Printing the sites...');
+  sites.forEach(e => {
+    console.log(JSON.stringify(e));
+  });
+
+  // console.log('Taking screenshot...');
+  // await page.screenshot({path: 'example.png'});
+
+  console.log('Closing the headless browser...');
+  await browser.close();
+};
+
+getSubjects();
